aa63ac5873c253c19c249338dc8c6f0a131af3f8
ASLI ARI <asliari2012@gmail.com>
2019-07-09 21:34:22 +0200

    Fix Apartmentdetails CSS grid

--- client/src/components/layout/ApartmentDetails.js
+++ client/src/components/layout/ApartmentDetails.js
@@ -40,73 +40,73 @@ class ApartmentDetails extends Component {
     const { post } = this.props.location.state;
     return (
       <div className="container section project-details">
-        <div className="card z-depth-0">
-          <div className="card-content">
-            {post ? (
-              <React.Fragment>
-                <span className="card-title">
-                  <div> {post.title}</div>
-                </span>
-                <Slider images={post.img} />
-                <div> {post.body}</div>
-              </React.Fragment>
-            ) : (
-              <Loading />
-            )}
-            <div
-              className="row col s12 center-align"
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <StartCalendar
-                placeholder={"Start Date"}
-                handleDate={this.handleStartDate}
-              />
-              <EndCalendar
-                placeholder={"End Date"}
-                handleDate={this.handleEndDate}
-              />
-              <div className="row col s4">
-                <Link
-                  to="/apartmentlistings"
-                  className="btn-large waves-effect waves-light red "
-                >
-                  <i className="material-icons left">card_travel</i>BOOK NOW
-                </Link>
-              </div>
-              <a
-                className="waves-effect waves-light btn-large "
-                onClick={this.props.checkAvailability}
-              >
-                Show me available dates
-              </a>
-
-              <a
-                className="waves-effect waves-light btn-large "
-                onClick={this.reserve}
-              >
-                Send my booking dates
-              </a>
-              {/* {mesaj ? <div> {mesaj}</div> : null} */}
+        {post ? (
+          <div className="section">
+            <div className="row" style={{ margin: "10px" }}>
+              <h4 className="grey-text text-darken-1 center-align">
+                {post.title}
+              </h4>
+            </div>
+            <Slider images={post.img} />
+            <div className="row" style={{ margin: "2em" }}>
+              {post.body}
             </div>
           </div>
-          <div className="card-action grey lighten-4 grey-text right-align">
-            {post ? (
-              <span>
-                <div>
-                  <h6>Property Owner: </h6>
-                  <div> {post.addedby}</div>
-                </div>
-                <div>date</div>
-              </span>
-            ) : (
-              <Loading />
-            )}
+        ) : (
+          <Loading />
+        )}
+        <div
+          className="row col s12 center-align"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <StartCalendar
+            placeholder={"Start Date"}
+            handleDate={this.handleStartDate}
+          />
+          <EndCalendar
+            placeholder={"End Date"}
+            handleDate={this.handleEndDate}
+          />
+          <div className="row col s4">
+            <Link
+              to="/apartmentlistings"
+              className="btn-large waves-effect waves-light red "
+            >
+              <i className="material-icons left">card_travel</i>BOOK NOW
+            </Link>
           </div>
+          <a
+            className="waves-effect waves-light btn-large "
+            onClick={this.props.checkAvailability}
+          >
+            Show me available dates
+          </a>
+
+          <a
+            className="waves-effect waves-light btn-large "
+            onClick={this.reserve}
+          >
+            Send my booking dates
+          </a>
+          {/* {mesaj ? <div> {mesaj}</div> : null} */}
+        </div>
+        <div className="card-action grey lighten-4 grey-text right-align">
+          {post ? (
+            <span>
+              <div>
+                <h6>Property Owner: </h6>
+                <div> {post.addedby}</div>
+              </div>
+              <div>date</div>
+            </span>
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     );
